815de48cdcc204441882be59b7df3572a7302a1c
DzeyEm <jmlaylo7@gmail.com>
2024-11-12 11:16:22 +0800

    Reminders Login of user

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 
 //TODO : create home page
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<Home> createState() => _StateHome();
 }
 
-class _StateHome extends State<Home>{
-
+class _StateHome extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       body: Center(
         child: Container(
           child: Column(
             children: [
               Text('Hello World'),
-              ElevatedButton(onPressed: (){
-                Navigator.pushReplacementNamed(context, '/login');
-              }, child: Text('Go to login page'))
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  child: Text('Go to login page'))
             ],
           ),
         ),
