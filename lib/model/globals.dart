ff9319c4d93689f268162a460c21dcd4267c1c46
Yajme <spartan.emjayrongavilla@gmail.com>
2024-11-25 14:09:06 +0800

    remove duplicates

--- lib/model/globals.dart
+++ lib/model/globals.dart
@@ -2,7 +2,6 @@ library my_prj.globals;
 
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trash_bin_app/api/firebase_api.dart';
 
 String token = '';
